41acf89fea60cf93773453335fc82e7df7b45756
Neil Thistlethwaite <neilt1111@yahoo.com>
2017-03-07 22:45:44 -0500

    add some ml code

--- marchmadness.py
+++ marchmadness.py
@@ -1,13 +1,15 @@
-import sklearn
+#import sklearn
 
 f = open("Teams.csv","r")
 s = f.read().split("\n")
 s.pop(0)
 s.pop(-1)
-teams = {}
+teamnames = {}
+teams = []
 for team in s:
     team = team.split(",")
-    teams[team[1]] = team[0]
+    teamnames[team[1]] = team[0]
+    teams.append(team[0])
 
 f = open("RegularSeasonDetailedResults.csv","r")
 s = f.read().split("\n")
@@ -35,3 +37,37 @@ for match in s:
                              match[WINNER], int(match[WINNERSCORE]), # team IDs intentionally left as strings so we 
                              match[LOSER], int(match[LOSERSCORE]),   # don't accidentally use them as continuous variables
                              attrs))
+
+simpledata = []
+learningrate = 0.05
+#for testing purposes
+for match in regseasonmatches:
+    if(match[SEASON] == 2003):
+        simpledata.append((match[WINNER], match[WINNERSCORE], match[LOSER], match[LOSERSCORE]))
+
+#attack, defense
+teamParameters = {}
+for team in teams
+    teamParameters[team] = [30,30]
+
+def calcerror(tparams, dat):
+    err = 0.0
+    for match in dat:
+        team1atk = tparams[match[0]][0]
+        team1def = tparams[match[0]][1]
+        team2atk = tparams[match[2]][0]
+        team2def = tparams[match[2]][1]
+        exp1score = team1atk - team2def
+        exp2score = team2atk - team1def
+        err += (exp1score - match[1])**2
+        err += (exp2score - match[3])**2
+    return err / len(dat)
+
+
+def calcgradient(tparams, dat):
+    gradient = {}
+    for team in tparams:
+        gradient[team] = [0,0]
+    for match in dat:
+        pass
+        
